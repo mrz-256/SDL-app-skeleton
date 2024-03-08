171b564f4d99d450677a3f11607adcc5f82e3258
mrz <mrz00001234@gmail.com>
2024-03-08 15:35:19 +0100

    wrote the virtual Scene class

--- game_lib/headers/scene_logic/Scene.h
+++ game_lib/headers/scene_logic/Scene.h
@@ -1,6 +1,22 @@
 #pragma once
 
+#include "../resources/global_resources.h"
+
+class SceneManager;
 
 class Scene {
+protected:
+    SceneManager* manager;
+
+public:
+
+    explicit Scene(SceneManager *manager);
+
+    virtual ~Scene();
+
+    virtual void update() = 0;
+
+    virtual void render(SDL_Renderer* renderer) = 0;
+
 
 };
--- game_lib/source/scene_logic/Scene.cpp
+++ game_lib/source/scene_logic/Scene.cpp
@@ -1,1 +1,8 @@
 #include "headers/scene_logic/Scene.h"
+#include "headers/scene_logic/SceneManager.h"
+
+
+Scene::Scene(SceneManager *manager) : manager(manager)
+{}
+
+Scene::~Scene() = default;
